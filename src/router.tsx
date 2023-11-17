ed214809a362161aae3b6708510f4995c3acaa3b
Leonardo Mayrink <leonardo.mayrink.dev@outlook.com>
2023-11-17 19:10:45 -0300

    testing routing

--- src/router.tsx
+++ src/router.tsx
@@ -8,12 +8,12 @@ import { BusinessPanel } from './pages/BusinessPanel';
 export function Router() {
     return (
         <Routes>
-            <Route path='/requests/' element={<RequestsExamples />}/>
-            <Route path='/forms/' element={<Forms />}/>
+            <Route path='/requests' element={<RequestsExamples />}/>
+            <Route path='/forms' element={<Forms />}/>
             <Route path='/example' element={<Example />}/>
             <Route path='/' element={<BusinessPanel />}/>
-            <Route path='/empresa/' element={<Empresa />}/>
-            <Route path='/painelnegocios/' element={<BusinessPanel />}/>
+            <Route path='/empresa' element={<Empresa />}/>
+            <Route path='/painelnegocios' element={<BusinessPanel />}/>
         </Routes>
     )
 }
